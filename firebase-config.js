a0fe77d69f5ae8ff351948be37e7d02bc54328e8
hrnano32-maker <hrnano32@gmail.com>
2026-08-24 14:07:34 +0700

    เชื่อม Firebase Firestore และ Storage

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js';
+import { getStorage } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCP-DLzpwuErHjXOOE9zhPptJX6a_uHcMU",
+  authDomain: "vehicle-mileage-control-dl.firebaseapp.com",
+  projectId: "vehicle-mileage-control-dl",
+  storageBucket: "vehicle-mileage-control-dl.firebasestorage.app",
+  messagingSenderId: "464190811569",
+  appId: "1:464190811569:web:f115ca22e07dc2761f2190",
+  measurementId: "G-FB8QGYYN50"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { db, storage };
